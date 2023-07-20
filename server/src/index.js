a13870c333acb0709bd803193c77aada00f9cbca
XieMS <minsan.x@doomeye.com>
2023-07-20 09:36:06 +0800

    adjust host

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json()); 
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "0.0.0.0",
   user: "root",
   password: "",
   database: "next_test",
